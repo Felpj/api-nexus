ece0dcaf5caf6a5b88e076b17a58fb58bbde5630
“Felpj” <felpcostajj@outlook.com>
2024-10-28 17:27:31 -0300

    bd

--- config/db.js
+++ config/db.js
@@ -3,15 +3,15 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,       // Nome do banco de dados
-  process.env.DB_USER,       // Usuário
-  process.env.DB_PASS || '',       // Senha
+  process.env.DB_NAME,       
+  process.env.DB_USER,       
+  process.env.DB_PASS || '',       
   {
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT || 'mysql',
     port: process.env.DB_PORT || 3306,
     define: {
-      timestamps: true,      // Cria automaticamente os campos createdAt e updatedAt
+      timestamps: true,      
     },
     pool: {
       max: 5,
@@ -19,7 +19,7 @@ const sequelize = new Sequelize(
       acquire: 30000,
       idle: 10000,
     },
-    logging: false,         // Desabilita logs SQL no console
+    logging: false,         
   }
 );
 
